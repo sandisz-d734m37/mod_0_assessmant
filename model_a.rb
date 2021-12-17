2ee46214f1311ba08764c2996936e35b311a31bf
Sandisz Thieme <sandiszthieme@gmail.com>
2021-12-16 17:08:22 -0700

    Add model_a.rb file

---
+++ model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  name: "Michael",
+  period: "Jurassic",
+  age: 1000000,
+  weight_in_lbs: 264927.4,
+  still_alive: false,
+  has_known_living_relative: false,
+  childrens_names: ["North", "Saint", "Bob Jr."],
+  parents_names: ["Jillian", "Bob Sr."]
+}
