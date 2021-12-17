75dae6af5afea09cc4f9f6ef973bb34d31abf6f9
Sandisz Thieme <sandiszthieme@gmail.com>
2021-12-16 17:13:15 -0700

    Add model_b.rb file

---
+++ model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  name: "Sam",
+  preferred_language: "Ruby",
+  age: 23,
+  years_of_experience: 4.75,
+  proficient_back_end_dev: true,
+  proficient_front_end_dev: true,
+  known_BE_languages: ["Ruby", "Python"],
+  known_FE_languages: ["JavaScript", "HTML", "CSS"]
+}
