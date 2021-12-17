eb287354799be6183432b5be7bcc6715963790a0
Sandisz Thieme <sandiszthieme@gmail.com>
2021-12-16 17:02:27 -0700

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = "Bee Movie"
+is_entertaining = true
+movie = "Dune"
+
+num_4 = 100
+num_6 = 3
+product = num_4 * num_6
+
+favorite_foods = ["Fried Rice", "Sesame Chicken", "Spaghetti"]
+favorite_foods.shift
+
+p movie.length
